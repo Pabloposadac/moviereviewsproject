696c87ca0704f5a8e224614c5fccfc525533fb9e
Pabloposadac <posada.pcp@gmail.com>
2024-07-31 20:37:07 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 def home(request):
     #return render(request, 'home.html')
     #return HttpResponse('<h1> Welcome to Home Page</h1>')
-    return render(request, 'home.html',{'name':'Pablo Posada Colorado'})
+    #return render(request, 'home.html',{'name':'Pablo Posada Colorado'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
 
 def about(request):
     return render(request, 'about.html')
